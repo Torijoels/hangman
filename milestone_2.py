764f90ace7addc778c04ad21eb1992d4b67d0114
Tori <Torijoels@gmail.com>
2023-01-20 21:08:16 +0000

    Ask the user for an input

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,14 @@
 import random
+#Task 1  and 2
 word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
 random.choice (word_list)
 word = random.choice (word_list)
 print (word)
+
+#Task 3
+#Ask the user for an input
+# %%
+guess = input ('Enter a single letter')
+print(guess)
+
+# %%
