19e61f47460a4e042e2e933bf6ea9ed707080fa8
Tori <Torijoels@gmail.com>
2023-01-21 13:25:57 +0000

    Validate user input using while loop

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,9 @@
 # %%
-#Task1 Interatively check if the input is a valid guess
-guess = input('guess a letter')
-while len(guess) == 1:
-      print ("Good guess!")
-      break
-else:
-    print ("Invalid letter. Please, enter a single alphabetical character.")
-
-
+#Task1 Continously run code until user inputs right letter
+while True:
+     guess = input('guess a letter: ')
+     if len(guess) == 1 :
+        print ("Good guess!")
+        break
+     print ("Invalid letter. Please, enter a single alphabetical character.")  
+# %%
