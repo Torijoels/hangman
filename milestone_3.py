b116664d825cd29cb79849e7e06d88ba9fbb83e7
Tori <Torijoels@gmail.com>
2023-03-14 21:49:53 +0000

    Adds Task3

--- milestone_3.py
+++ milestone_3.py
@@ -1,11 +1,6 @@
 # %%
 #Task1 Continously run code until user inputs right letter
-while True:
-     guess = input('guess a letter: ')
-     if len(guess) == 1 :
-        print ("Good guess!")
-        break
-     print ("Invalid letter. Please, enter a single alphabetical character.")  
+
 # %%
 #Task 2 check whether the guess is in the word
 word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
@@ -17,4 +12,25 @@ if guess in word:
     print ("Good guess! {} is in the word.".format(guess))
 else:
     print ("Sorry, {} is not in the word. Try again.".format(guess))
+# %%
+#Task 3 create the function to run tests
+def check_guess (guess):
+    guess.lower()
+    guess = input ('Enter a single letter')
+    if guess in word:
+        print ("Good guess! {} is in the word.".format(guess))
+    else:
+        print ("Sorry, {} is not in the word. Try again.".format(guess))
+
+def ask_for_input ():
+    while True:
+        guess = input('guess a letter: ')
+        if len(guess) == 1 :
+            print ("Good guess!")
+        break
+    print ("Invalid letter. Please, enter a single alphabetical character.") 
+
+ 
+
+ 
 # %%
