1885034d3539bdd92d8d11dde452b113e4788317
Tori <Torijoels@gmail.com>
2023-03-21 19:29:21 +0000

    task3 define fucntions

--- milestone_3.py
+++ milestone_3.py
@@ -3,39 +3,39 @@
 
 # %%
 #Task 2 check whether the guess is in the word
-word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
-import random
-random.choice (word_list)
-word = random.choice (word_list)
-guess = input ('Enter a single letter')
-if guess in word:
-    print ("Good guess! {} is in the word.".format(guess))
-else:
-    print ("Sorry, {} is not in the word. Try again.".format(guess))
+
 # %%
+
 #Task 3 create the function to run tests
-guess = input ('Enter a single letter')
-def check_guess (guess):
-     word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
-     import random
-     random.choice (word_list)
-     word = random.choice (word_list)
-     guess = input ('Enter a single letter')
-     if guess in word:
-        print ("Good guess! {} is in the word.".format(guess))
-     else:
-        print ("Sorry, {} is not in the word. Try again.".format(guess))
+def check_guess ():
+    guess = input ('Enter a single letter')
+    guess.lower()
+    word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
+    import random
+    random.choice (word_list)
+    word = random.choice (word_list)
+    if guess in word:
+       print ("Good guess! {} is in the word.".format(guess))
+    else:
+       print ("Sorry, {} is not in the word. Try again.".format(guess))
+# %%
 
 def ask_for_input ():
-    while True:
-        guess = input('guess a letter: ')
-        if len(guess) == 1 :
+      while True:
+         guess = input('guess a letter: ')
+         if len(guess) == 1 :
             print ("Good guess!")
-        break
-    print ("Invalid letter. Please, enter a single alphabetical character.") 
+         break
+         print ("Invalid letter. Please, enter a single alphabetical character.")  
+      check_guess ('c') 
 
-
- 
-
- 
 # %%
+
+ask_for_input()
+
+
+
+# %%
+#Task 3 create the function to run tests
+
+ 
