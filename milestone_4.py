c2e022d717c504e82d41b93f0cae7b4bd01c1361
Tori <Torijoels@gmail.com>
2023-05-07 14:28:43 +0100

    Task2 Milestone4

--- milestone_4.py
+++ milestone_4.py
@@ -1,30 +1,4 @@
-
 import random
-def check_guess ():
-    guess = input ('Enter a single letter')
-    guess.lower()
-    word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
-    import random
-    random.choice (word_list)
-    word = random.choice (word_list)
-    if guess in word:
-       print ("Good guess! {} is in the word.".format(guess))
-    else:
-       print ("Sorry, {} is not in the word. Try again.".format(guess))
-# %%
-
-def ask_for_input ():
-      while True:
-         guess = input('guess a letter: ')
-         if len(guess) == 1 :
-            print ("Good guess!")
-         break
-         print ("Invalid letter. Please, enter a single alphabetical character.")  
-      check_guess ('c') 
-
-# %%
-ask_for_input()
-
 #%%
 class HangMan:
     def __init__(self, word_list, num_lives=5):
@@ -35,5 +9,28 @@ class HangMan:
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
+        def check_guess (guess):
+            guess.lower()
+            word_list = ['Orange', 'Mango', 'Banana', 'Pear', 'Tangerine']
+            random.choice (word_list)
+            word = random.choice (word_list)
+            if guess in word:
+                print ("Good guess! {} is in the word.".format(guess))
+            else:
+                print ("Sorry, {} is not in the word. Try again.".format(guess))
+        
+        def ask_for_input ():
+            while True:
+              guess = input('guess a letter: ')
+              if len(guess) != 1 :
+                print ("Invalid letter. Please, enter a single alphabetical character.")  
+              elif guess in self.list_of_guesses :
+                print ("You already tried that letter!")
+              else:
+                check_guess (guess)
+                guess = self.list_of_guesses
+        #%%
+        ask_for_input ()
 
 
+# %%
