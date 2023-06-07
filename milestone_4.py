2a6e36308552be4099e26e558841b722fd87b46d
Tori <Torijoels@gmail.com>
2023-06-07 19:59:47 +0100

    Modify milestone_4

--- milestone_4.py
+++ milestone_4.py
@@ -4,36 +4,34 @@ import random
 class HangMan:
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice (word_list)
-        self.word_guessed = [[] for i in range(10)]
+        self.word_guessed = []
         self.num_letters = len(set(self.word_guessed))
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []  
-    def check_guess (guess):
-        guess = input('guess a letter: ')
-        guess_lower = guess.lower()
-        word_list = ['orange', 'mango', 'banana', 'pear', 'tangerine']
-        if guess in guess.word:
-           print (f"Good guess! {guess} is in the word.")
-        else:
-           print (f"Sorry, {guess} is not in the word. Try again.")
+
+    def check_guess (self, guess):
+         guess = input('guess a letter: ')
+         guess_lower = guess.lower()
+         word_list = ['orange', 'mango', 'banana', 'pear', 'tangerine']
+         if guess in guess:
+            print ("Good guess! {guess} is in the word.")
+         else:
+            print ("Sorry, {guess} is not in the word. Try again.")
         
-    def ask_for_input (self):
-            while True:
-              guess = input('guess a letter: ')
-              if len(guess) != 1 and guess not in "abcdefghijklmnopqrstuvwxyz":
-                 print ("Invalid letter. Please, enter a single alphabetical character.")  
-              elif guess in self.list_of_guesses :
+    def ask_for_input (self,):
+         while True:
+            guess = input('guess a letter: ')
+            if len(guess) != 1 or guess not in "abcdefghijklmnopqrstuvwxyz":
+               print ("Invalid letter. Please, enter a single alphabetical character.")  
+            elif guess in self.list_of_guesses :
                 print ("You already tried that letter!")
-              else:
-                calling_checkguess = HangMan ()
-                calling_checkguess.check_guess (guess)
-              self.list_of_guesses.append(guess)
-        
-#%%
-tmp = HangMan()
+            else:
+                self.check_guess(guess)
+word_list = ['orange', 'mango', 'banana', 'pear', 'tangerine']
+tmp = HangMan(word_list)
 print (tmp.ask_for_input())      
       
 
 
-# %%
+
